55f37d3a6a306bf8d70ff08038a153afa33d3e5c
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2023-12-24 18:40:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ b = float(input('Please enter the edge b: '))
 c = float(input('Please enter the edge c: '))
 if a+b > c and a+c > b and b+c > a:
     print('\nThis is really a triangle!\n')
+    
     p = (a+b+c)/2
     cosA = (b*b+c*c-a*a)/(2*b*c)
     cosB = (a*a+c*c-b*b)/(2*a*c)
@@ -14,6 +15,7 @@ if a+b > c and a+c > b and b+c > a:
     degA = float(radA*180/pi)
     degB = float(radB*180/pi)
     degC = float(radC*180/pi)
+    
     print('C = %f.' % float(2*p))
     print('S = %f.\n' % sqrt(p*(p-a)*(p-b)*(p-c)))
 
