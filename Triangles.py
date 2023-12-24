c2b06b27f3e984cbc8a017b20644f1708edb6742
Aviation Engineer <109074663+YBCA7@users.noreply.github.com>
2023-12-24 16:18:39 +0800

    Add files via upload

---
+++ Triangles.py
@@ -0,0 +1,21 @@
+from math import sqrt, acos, pi
+a = float(input('Please enter the edge a: '))
+b = float(input('Please enter the edge b: '))
+c = float(input('Please enter the edge c: '))
+if a+b > c and a+c > b and b+c > a:
+    print('\nThis is really a triangle!\n')
+    p = (a+b+c)/2
+    cosA = (b*b+c*c-a*a)/(2*b*c)
+    cosB = (a*a+c*c-b*b)/(2*a*c)
+    cosC = (a*a+b*b-c*c)/(2*a*b)
+    print('C = %f.' % float(2*p))
+    print('S = %f.\n' % sqrt(p*(p-a)*(p-b)*(p-c)))
+    print('cosA = %f.' % cosA)
+    print('cosB = %f.' % cosB)
+    print('cosC = %f.\n' % cosC)
+    print('∠A = %f.' % float(acos(cosA)*180/pi))
+    print('∠B = %f.' % float(acos(cosB)*180/pi))
+    print('∠C = %f.' % float(acos(cosC)*180/pi))
+else:
+    print('This is not really a triangle.')
+input()
